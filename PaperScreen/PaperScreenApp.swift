4f0e42b22bd8fccce8cc6eadc1344275030eb25d
bearbobs <kapoor.anuj0@gmail.com>
2026-07-20 08:26:06 +0200

    Initial Commit

---
+++ PaperScreen/PaperScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperScreenApp.swift
+//  PaperScreen
+//
+//  Created by anuj on 20.07.26.
+//
+
+import SwiftUI
+
+@main
+struct PaperScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
